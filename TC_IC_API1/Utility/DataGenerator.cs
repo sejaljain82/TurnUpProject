1cad203af7b318c8fad0f6f9901465a3ebd45ef7
sejaljain82 <rosejain@gmail.com>
2021-08-01 23:07:49 +1200

    Utility BillingOrderTestData.csv is the data CSV file for Data Driven functions DataGenerator.cs has function for generting Random Data for Data Driven Funtion

---
+++ TC_IC_API1/Utility/DataGenerator.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TC_IC_API1.Utility
+{
+    class DataGenerator
+    {
+        static string a1 = "abcdefghijklmnopqrstuvwxyz";
+        static string a2 = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        static string num = "0123456789";
+        public static string RandomString(int len = 10)
+        {
+            return TestContext.CurrentContext.Random.GetString(len, $"{a1}{a2}");
+        }
+        public static string  RandomStringint(int len = 10)
+        {
+            return TestContext.CurrentContext.Random.GetString(len, $"{num}");
+        }
+        public static string FullName => Faker.Name.FullName();
+        public static string LastName => Faker.Name.FullName();
+
+
+    }
+}
