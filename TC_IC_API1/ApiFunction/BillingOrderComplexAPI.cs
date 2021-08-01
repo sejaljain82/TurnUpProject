680d0fddef281dceba49e2d36cc6f45a19e5cf8c
sejaljain82 <rosejain@gmail.com>
2021-08-01 22:12:19 +1200

    Class has functionts that perform CRUD on omline API

---
+++ TC_IC_API1/ApiFunction/BillingOrderComplexAPI.cs
@@ -0,0 +1,76 @@
+﻿using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TC_IC_API1.ApiFunction
+{
+    class BillingOrderComplexAPI
+    {
+        
+            private readonly string BaseUrl = "http://api.qaauto.co.nz/api/v1";
+
+            //Funtion to Read all the Data from API 
+            public IRestResponse GetAllOrder()
+            {
+                var client = new RestClient($"{BaseUrl}/billingOrders");
+                var request = new RestRequest(Method.GET);
+
+                IRestResponse response = client.Execute(request);
+                return response;
+            }
+            //Funtion to read Data from API by giving the Order Id 
+            public IRestResponse GetOrderById(string id)
+            {
+                var client = new RestClient($"{BaseUrl}/billingOrders/{id}");
+                var request = new RestRequest(Method.GET);
+
+                IRestResponse response = client.Execute(request);
+                return response;
+            }
+
+            //Funtion to Delete Data from API by giving the Order Id 
+            public IRestResponse DeleteOrderById(string id)
+            {
+                var client = new RestClient($"{BaseUrl}/billingOrders/{id}");
+                var request = new RestRequest(Method.DELETE);
+
+                IRestResponse response = client.Execute(request);
+                return response;
+            }
+
+            //Function to Create/Post a new record in the Billing Order API 
+            public IRestResponse PostOrder(string Body)
+            {
+                var client = new RestClient($"{BaseUrl}/billingOrders");
+                var request = new RestRequest(Method.POST);
+
+                //Setting Header so that we know data is in jason format
+                request.AddHeader("Content-Type", "application/json");
+
+                //Sending the data to the API which is passed in the string named as Body
+                request.AddParameter("application/json", Body, ParameterType.RequestBody);
+
+                IRestResponse response = client.Execute(request);
+                return response;
+            }
+
+            //Function to Update/Put a record in the Billing Order API using Id
+            public IRestResponse PutOrder(string Body, string id)
+            {
+                var client = new RestClient($"{BaseUrl}/billingOrders/{id}");
+                var request = new RestRequest(Method.PUT);
+
+                //Setting Header so that we know data is in jason format
+                request.AddHeader("Content-Type", "application/json");
+
+                //Sending the data to the API which is passed in the string named as Body
+                request.AddParameter("application/json", Body, ParameterType.RequestBody);
+
+                IRestResponse response = client.Execute(request);
+                return response;
+            }
+        }
+
+    }
+
